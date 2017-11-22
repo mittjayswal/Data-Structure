6add70b31e1a73861ff6b09c7581558798f2861a
mittjayswal <mittjayswal@gmail.com>
2017-11-21 16:48:46 -0800

    bubble sort

---
+++ Sorting Algorithms/BubbleSort.java
@@ -0,0 +1,25 @@
+package sorting;
+
+public class BubbleSort {
+
+	public static void main(String[] args) {
+		int[] arr = {6,8,10,34,4,2,0};
+		bubbleSort(arr);
+		for (int i=0; i<arr.length; i++){
+			System.out.print(arr[i]+" ");
+		}
+		
+	}
+
+	private static void bubbleSort(int[] arr) {
+		for(int i=0; i<arr.length; i++){
+			for (int j=0; j<(arr.length-1-i); j++){
+				if (arr[j] > arr[j+1]){
+					int temp = arr[j];
+					arr[j] = arr[j+1];
+					arr[j+1] = temp;
+				}
+			}
+		}
+	}
+}
